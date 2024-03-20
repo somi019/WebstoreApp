17f3e9d2410ce2a2809b7d345ba13df0bab0188d
somi019 <milosdjordjevicc19@gmail.com>
2024-03-20 17:47:49 +0100

    Kreiranje projekta i CatalogApi
    Added mongodb, put some data in it and wrote methods for Catalog

---
+++ README.md
@@ -0,0 +1,2 @@
+# matfRazvojSoftvera2
+ 
---
+++ Webstore/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext() {
+            // Ovaj kontekst je zaduzen za povezivanje sa bazom podataka
+            var client = new MongoClient("mongodb://localhost:27017");
+            // var isto kao auto u cpp, automatska dedukcija tipa
+            var database = client.GetDatabase("CatalogDB");
+
+            Products = database.GetCollection<Product>("Products");
+            // povezali smo se na bazu podataka i uzeli jednu kolekciju podataka iz nje
+            CatalogContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
