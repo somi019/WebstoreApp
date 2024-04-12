18a1925831108f29bb3ed9776d800ad94cbd4a30
somi019 <milosdjordjevicc19@gmail.com>
2024-04-12 10:13:40 +0200

    Added a product repository with get  products method

---
+++ Webstore/Services/Catalog/Catalog.API/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿using Catalog.API.Entities;
+
+namespace Catalog.API.Repositories.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+    }
+}
---
+++ Webstore/Services/Catalog/Catalog.API/Repositories/Interfaces/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using Catalog.API.Data;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Repositories.Interfaces
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ICatalogContext _context;
+
+        public ProductRepository(ICatalogContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            // treba da se importuje mongo, u njemu je metoda Find koji vraca IFindFluent koji nam odgovara
+            return await _context.Products.Find(p=>true).ToListAsync();
+            // spisak proizvoda ce pretvoriti u listu
+
+        }
+
+    }
+}
