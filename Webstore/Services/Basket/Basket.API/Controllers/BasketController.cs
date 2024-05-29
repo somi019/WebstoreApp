dec2bf694ff7da9fff3b35e46c7dabc73da7b43e
somi019 <milosdjordjevicc19@gmail.com>
2024-05-29 18:13:41 +0200

    Zadnji commit rs2

---
+++ Webstore/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,42 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class BasketController : Controller
+    {
+        private readonly IBasketRepository _repository;
+
+        public BasketController(IBasketRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+        [HttpGet("{username}")]
+        [ProducesResponseType(typeof(ShoppingCart), StatusCodes.Status200OK)]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string username)
+        { 
+            var basket =  await _repository.GetBasket(username);
+            return Ok(basket ?? new ShoppingCart(username));
+        }
+        [HttpPut]
+        [ProducesResponseType(typeof(ShoppingCart), StatusCodes.Status200OK)]
+        
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket) 
+        {
+            return Ok(await _repository.UpdateBasket(basket));
+            
+        }
+        [HttpDelete("{username}")]
+        [ProducesResponseType(typeof(void), StatusCodes.Status200OK)]
+
+        public async Task<ActionResult> DeleteBasket(string username)
+        {
+            await _repository.DeleteBasket(username);
+            return Ok();
+        }
+
+    }
+}
