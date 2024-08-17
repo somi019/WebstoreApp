c77b92b2b7f7c6f61dc0fdbb12a1f79872070550
somi019 <milosdjordjevicc19@gmail.com>
2024-08-17 12:32:05 +0200

    Implemented server-side gRPC communication

--- Webstore/Services/Discount/Discount.GRPC/Program.cs
+++ Webstore/Services/Discount/Discount.GRPC/Program.cs
@@ -1,3 +1,7 @@
+
+using Discount.Common.DTOs;
+using Discount.Common.Extensions;
+using Discount.GRPC.Protos;
 using Discount.GRPC.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,11 +11,24 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddDiscountCommonServices();
+
+// Mapiranja : CouponDTO -> GetDiscountResponse,
+// CouponDTO -> GetRandomDiscountsResponse.Types.Coupon
+builder.Services.AddAutoMapper(configuration =>
+{
+    configuration.CreateMap<CouponDTO,GetDiscountResponse>().ReverseMap();
+    configuration.CreateMap<CouponDTO, GetRandomDiscountsResponse.Types.Coupon>().ReverseMap();
+});
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+
+// koristi ovaj GRPC servis
+app.MapGrpcService<CouponService>();
+
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
