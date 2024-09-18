90e5a1f9e4a3f814204c295f66a4addd74c75ad5
somi019 <milosdjordjevicc19@gmail.com>
2024-09-18 12:37:12 +0200

    Implemented OrderContext
    ~Added OrderContext
    ~Added OrderContextSeed

---
+++ Webstore/Services/Ordering/Ordering.Infrastructure/Persistance/OrderContext.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Aggregates;
+using Ordering.Domain.Common;
+using Ordering.Infrastructure.Persistance.EntityConfigurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistance
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions options) : base(options) 
+        {
+        }
+        public DbSet<Order> Orders { get; set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<EntityBase>()) 
+            {
+                // za sve koji nasledjuju EntityBase(entiteti i agregati)
+                switch (entry.State) 
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedBy = "rs2";
+                        entry.Entity.CreatedDate = DateTime.Now;
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedBy = "rs2";
+                        entry.Entity.LastModifiedDate = DateTime.Now;
+                        break;
+                }
+            } 
+            return base.SaveChangesAsync(cancellationToken);
+            //treba da se ostavi uvek base poziv
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new OrderEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderItemEntityTypeConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
+// svaka baza treba da ima 1 kontekst
