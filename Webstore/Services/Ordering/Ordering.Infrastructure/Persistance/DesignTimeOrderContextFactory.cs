d8cb86e9118d5d83f0360150f45caf67e4816240
somi019 <milosdjordjevicc19@gmail.com>
2024-09-18 18:38:06 +0200

    Create DesignTimeOrderContextFactory.cs

---
+++ Webstore/Services/Ordering/Ordering.Infrastructure/Persistance/DesignTimeOrderContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistance
+{
+    public class DesignTimeOrderContextFactory : IDesignTimeDbContextFactory<OrderContext>
+    {
+        public OrderContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<OrderContext>();
+            optionsBuilder.UseSqlServer();
+            return new OrderContext(optionsBuilder.Options);
+        }
+    }
+}
