2d5d9bb7cda9c88d84e5c570da4cb85c5fe503f9
somi019 <milosdjordjevicc19@gmail.com>
2024-09-18 12:36:10 +0200

    Added configurations
    ~configuration classes for Order and OrderItem

---
+++ Webstore/Services/Ordering/Ordering.Infrastructure/Persistance/EntityConfigurations/OrderEntityTypeConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Aggregates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistance.EntityConfigurations
+{
+    public class OrderEntityTypeConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.ToTable("Orders");
+            builder.HasKey(o => o.Id);
+            builder.Property(o => o.Id).UseHiLo();
+            // OrderItem se sastoji samo iz obicnih polja (stringovi,intovi,decimal,itd.)
+            // Ali Order u sebi ima vrednosni objekat Address pored toga
+            // treba reci : Order je vlasnik tog vrednosnog objekta
+            builder.OwnsOne(o => o.Address, a =>
+            {
+                // adresa bi trebalo da ima neki strani kljuc da bi se znalo na koju se porudzbinu odnosi
+                a.Property<int>("OrderId").UseHiLo("orderseq");
+                // istu sekvencu "orderseq" koristim za generisanje privatnih kljuceva za order
+                a.WithOwner();
+            });
+
+            var navigation = builder.Metadata.FindNavigation(nameof(Order.OrderItems));
+            navigation.SetPropertyAccessMode(PropertyAccessMode.Field);
+            //nadji public OrderItems i postavi da se pristupa polju _orderItems
+
+        }
+    }
+}
---
+++ Webstore/Services/Ordering/Ordering.Infrastructure/Persistance/EntityConfigurations/OrderItemEntityTypeConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistance.EntityConfigurations
+{
+    public class OrderItemEntityTypeConfiguration : IEntityTypeConfiguration<OrderItem>
+    {
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            builder.ToTable("OrderItems");
+            // napravi tabelu OrderItems
+
+            builder.HasKey(o => o.Id);
+            // polje koje se zove Id je primarni kljuc ove tabele
+
+            builder.Property(o => o.Id).UseHiLo("orderitemseq");
+            // koristi HiLo algoritam za davanje Id-a
+
+            builder.Property<string>("ProductId")
+                .HasColumnType("VARCHAR(24)")
+                .HasColumnName("ProductId")
+                .IsRequired();
+            // dodajemo kolonu, koja ima tip VARCHAR(24)
+            // ima ime "ProductId"(mogli smo bilo koje drugo)
+            // i required je, obavezna
+
+            // uradi do kraja za sve Property-e
+           
+
+
+        }
+    }
+}
