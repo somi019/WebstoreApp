3f7fc1641be98d000e09d3831c7ae11a58a5af1c
somi019 <milosdjordjevicc19@gmail.com>
2024-09-18 12:37:33 +0200

    Implemented OrderRepository

--- Webstore/Services/Ordering/Ordering.Application/Contracts/Persistence/IAsyncRepository.cs
+++ Webstore/Services/Ordering/Ordering.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -29,8 +29,8 @@ namespace Ordering.Application.Contracts.Persistence
             bool disableTracking = true);
         Task<T> GetByIdAsync(int id);
         Task<T> AddAsync(T entity);
-        Task<T> UpdateAsync(T entity);
-        Task<T> DeleteAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
 
 
     }
---
+++ Webstore/Services/Ordering/Ordering.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Aggregates;
+using Ordering.Infrastructure.Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Repositories
+{
+    public class OrderRepository : RepositoryBase<Order>, IOrderRepository
+    {
+        public OrderRepository(OrderContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersByUsername(string username)
+        {
+            return await _dbContext.Orders
+                .Where(o => o.BuyerUsername == username)
+                .Include(o => o.OrderItems)
+                .ToListAsync();
+            // bez Include ne bi radilo, tu ukljucujemo i OrderItems sa left join
+        }
+    }
+}
+// Ovo je repozitorijum za Order, samo smo mu dodali jos jednu metodu pored RepositoryBase
+// koja je za uzimanje Ordera za jedan Username
+// fora u DDD je da imamo sto vise baznih stvari koje koristimo svuda da ne bi ponavljali kod
+// zato kad dodajes nesto novo i implementiras neki feature vrlo se lagano doda
