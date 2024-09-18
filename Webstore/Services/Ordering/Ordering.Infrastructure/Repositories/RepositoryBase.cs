6623e49981528ededa0715d5bfb65fb22acdb4e2
somi019 <milosdjordjevicc19@gmail.com>
2024-09-18 12:35:32 +0200

    Project changes

---
+++ Webstore/Services/Ordering/Ordering.Infrastructure/Repositories/RepositoryBase.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Common;
+using Ordering.Infrastructure.Persistance;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Repositories
+{
+    public class RepositoryBase<T> : IAsyncRepository<T> where T : AggregateRoot
+    {
+        protected readonly OrderContext _dbContext;
+
+        public RepositoryBase(OrderContext dbContext)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+
+        public virtual async Task<IReadOnlyList<T>> GetAllAsync()
+        {
+            return await _dbContext.Set<T>().ToListAsync();
+            // dohvati mi skup entiteta tipa T
+        }
+
+        public virtual async Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbContext.Set<T>().Where(predicate).ToListAsync();
+            // dohvati mi skup entiteta tipa T gde se zadovoljava predikat
+        }
+
+        public virtual async Task<IReadOnlyList<T>> GetAsync(
+            Expression<Func<T, bool>> predicate = null, Func<IQueryable<T>,
+                IOrderedQueryable<T>> orderBy = null, string? includeString = null,
+                bool disableTracking = true)
+        {
+            IQueryable<T> query = _dbContext.Set<T>();
+            if (disableTracking) query = query.AsNoTracking();
+            
+            if (!string.IsNullOrWhiteSpace(includeString)) query = query.Include(includeString);
+
+            if (predicate != null) query = query.Where(predicate);
+
+            if (orderBy != null) return await orderBy(query).ToListAsync();
+
+            return await query.ToListAsync();
+        }
+        // razlika izmedju IEnumerable i IQueryable
+        // - IQueryable podrzava lazy loading, dok IEnumerable ne moze
+        // primer : -IEnumerable : - uzima celu tabelu, ucitava u memoriju, tek onda primenjuje filter na klijentu
+        //          -IQueryable : - izvrsava select query na serverskoj strani sa svim filterima
+
+
+        public virtual async Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null, Func<IQueryable<T>,
+            IOrderedQueryable<T>> orderBy = null,
+            List<Expression<Func<T, object>>> includes = null,
+            bool disableTracking = true)
+        {
+            // Ovo  List<Expression<Func<T, object>>> je kad bi pored OrderItems
+            // imali jos nekoliko kolekcija u Order
+            // inace samo treba includeString
+
+            IQueryable<T> query = _dbContext.Set<T>();
+            if (disableTracking) query = query.AsNoTracking();
+
+            if (includes != null) query = includes.Aggregate(query, (current,include) => current.Include(include));
+
+            if (predicate != null) query = query.Where(predicate);
+
+            if (orderBy != null) return await orderBy(query).ToListAsync();
+
+            return await query.ToListAsync();
+        }
+
+        public virtual async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbContext.Set<T>().FindAsync(id);
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            _dbContext.Set<T>().Add(entity);
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+        public async Task UpdateAsync(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
+        public async Task DeleteAsync(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+
+    }
+}
+// Ovo je najopstiji moguci RepositoryBase, dobar kod za reusability
