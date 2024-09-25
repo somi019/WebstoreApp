126a2c14d9c860453f33db7c8a70c345043cc7be
somi019 <milosdjordjevicc19@gmail.com>
2024-09-25 10:36:44 +0200

    Added role configuration

--- Webstore/Security/IdentityServer/Data/ApplicationContext.cs
+++ Webstore/Security/IdentityServer/Data/ApplicationContext.cs
@@ -1,4 +1,5 @@
-﻿using IdentityServer.Entities;
+﻿using IdentityServer.Data.EntityTypeConfiguration;
+using IdentityServer.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,6 +9,12 @@ namespace IdentityServer.Data
     {
         public ApplicationContext(DbContextOptions options) : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfiguration(new RoleConfiguration());
         }
     }
 }
